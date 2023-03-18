8bc19eef3780bf0596f24512a331d3716e518f82
RobertDavis1408 <125427151+RobertDavis1408@users.noreply.github.com>
2023-03-17 20:25:57 -0400

    initial deploy

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const metersToFeet = 3.281 
+const litersToGallons = 0.264 
+const kilogramToPounds = 2.204
+
+const inputBoxEl = document.getElementById("input-box")
+const convertBtn = document.querySelector("#convert-btn")
+let lengthOutputEL = document.querySelector("#length-output")
+const volumeOutputEl = document.querySelector("#volume-output")
+const massOutputEl = document.querySelector("#mass-output")
+
+convertBtn.addEventListener("click", function(){
+    inputBoxEl.textContent = "0";
+   inputEl= inputBoxEl.value;
+   lengthOutputEL.textContent =`${inputEl} meters = ${(inputEl * metersToFeet).toFixed(3)} feet | 
+   ${inputEl} feet = ${(inputEl / metersToFeet).toFixed(3)} meters`;
+   volumeOutputEl.textContent  =`${inputEl} liters = ${(inputEl * litersToGallons).toFixed(3)} gallons | 
+   ${inputEl} gallons = ${(inputEl / litersToGallons).toFixed(3)} liters`;
+   massOutputEl.textContent =`${inputEl} kilos = ${(inputEl * litersToGallons).toFixed(3)} pounds | 
+   ${inputEl} pounds = ${(inputEl / kilogramToPounds).toFixed(3)} kilos`    
+    
+    document.getElementById("input-box").value = "";
+})
+
+
+
+
+
+
+
+
+
+
+
